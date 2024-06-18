d628c29516efeb588329750d475385384322183b
Dami-Bombara <bombaradamian@gmail.com>
2024-06-18 18:13:39 +0200

    Add form to create a new culer with redirects

--- app/controllers/culerify_controller.rb
+++ app/controllers/culerify_controller.rb
@@ -2,4 +2,20 @@ class CulerifyController < ApplicationController
   def index
     @culer = Culer.all
   end
+
+  def show
+    @culer = Culer.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @culer = Culer.create(name: params[:name], age: params[:age], country: params[:country], position: params[:position],
+                          kit_number: params[:kit_number])
+
+    redirect_to culerify_url
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'culerify', to: 'culerify#index'
+  get 'culerify/new', to: 'culerify#new'
+  get 'culerify/:id', to: 'culerify#show'
+  post 'culerify', to: 'culerify#create'
 
   root 'home#index'
 end
