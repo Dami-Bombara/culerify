81520284153ec93af1b49a089ac473adee32d6ae
Dami-Bombara <bombaradamian@gmail.com>
2024-06-19 12:47:40 +0200

    Add table to visualize content & links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'culerify', to: 'culerify#index'
   get 'culerify/new', to: 'culerify#new'
-  get 'culerify/:id', to: 'culerify#show'
+  get 'culerify/:id', to: 'culerify#show', as: 'culer'
   post 'culerify', to: 'culerify#create'
 
   root 'home#index'
